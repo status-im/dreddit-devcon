20f0749e8697df0e267120f8ab31c8b82876fc4a
Pascal Precht <pascal.precht@gmail.com>
2018-10-31 10:55:08 +0100

    Step 2 - Write unit tests for DReddit contract

--- test/contract_spec.js
+++ test/contract_spec.js
@@ -28,11 +28,16 @@ contract("DReddit contract", function () {
   this.timeout(0);
 
   it("should be able to create a post and receive it via contract event", async function () {    
-    // TODO: 
+    let receipt = await DReddit.methods.create(web3.utils.fromAscii(ipfsHash)).send();
+    const event = receipt.events.NewPost;
+    postId = event.returnValues.postId;
+    assert.equal(web3.utils.toAscii(event.returnValues.description), ipfsHash);
   });
 
   it("post should have correct data", async function (){
-    // TODO: 
+    const post = await DReddit.methods.posts(postId).call();
+    assert.equal(web3.utils.toAscii(post.description), ipfsHash);
+    assert.equal(post.owner, accounts[0]);
   });
 
   it("one post should be registered", async function () {
@@ -57,7 +62,12 @@ contract("DReddit contract", function () {
   });
 
   it("should't be able to vote twice", async function () {
-    // TODO: 
+    try {
+      const receipt = await DReddit.methods.vote(postId, 1).send();
+      assert.fail('should have reverted before');
+    } catch (error){
+        assert(error.message.search('revert') > -1, 'Revert should happen');
+    }
   });
 
 });
